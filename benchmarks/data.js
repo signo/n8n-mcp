181754dbff79e63f4c422ab5d975e9583e3584a5
github-action-benchmark <github@users.noreply.github.com>
2025-12-26 14:04:32 +0000

    add n8n-mcp Benchmarks (customSmallerIsBetter) benchmark result for 3a4b67e7c2ef4f8fcddd5edc02b0f36410c3449b

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1766752200261,
+  "lastUpdate": 1766757872504,
   "repoUrl": "https://github.com/signo/n8n-mcp",
   "entries": {
     "n8n-mcp Benchmarks": [
@@ -54,6 +54,37 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/signo/n8n-mcp/commit/1a2f07a1dd050d481d52b72d5a1d7fef13560d57"
         },
         "date": 1766752199637,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sample - array sorting - small",
+            "value": 0.0136,
+            "range": "0.3096",
+            "unit": "ms",
+            "extra": "73341 ops/sec"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "signodev@signocreativo.com",
+            "name": "Signo Creativo",
+            "username": "signo"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3a4b67e7c2ef4f8fcddd5edc02b0f36410c3449b",
+          "message": "Update volume configuration in docker-compose.yml",
+          "timestamp": "2025-12-26T15:02:48+01:00",
+          "tree_id": "6b065992f16b154020f863d551b243a81c24f0d9",
+          "url": "https://github.com/signo/n8n-mcp/commit/3a4b67e7c2ef4f8fcddd5edc02b0f36410c3449b"
+        },
+        "date": 1766757872227,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
