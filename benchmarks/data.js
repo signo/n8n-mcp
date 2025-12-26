6644f529863bf1ced312b506739151e2b9e0b438
github-action-benchmark <github@users.noreply.github.com>
2025-12-26 12:26:04 +0000

    add n8n-mcp Benchmarks (customSmallerIsBetter) benchmark result for 6ffbe2536fc99e280ab8c2ce474c63d0b04077a7

---
+++ benchmarks/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1766751964676,
+  "repoUrl": "https://github.com/signo/n8n-mcp",
+  "entries": {
+    "n8n-mcp Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "signodev@signocreativo.com",
+            "name": "Signo Creativo",
+            "username": "signo"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6ffbe2536fc99e280ab8c2ce474c63d0b04077a7",
+          "message": "Merge branch 'czlonkowski:main' into main",
+          "timestamp": "2025-12-26T13:23:58+01:00",
+          "tree_id": "2753546cf1c2dcbd58643fcbf8f9942a773531aa",
+          "url": "https://github.com/signo/n8n-mcp/commit/6ffbe2536fc99e280ab8c2ce474c63d0b04077a7"
+        },
+        "date": 1766751963809,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sample - array sorting - small",
+            "value": 0.0136,
+            "range": "0.3096",
+            "unit": "ms",
+            "extra": "73341 ops/sec"
+          }
+        ]
+      }
+    ]
+  }
+}
